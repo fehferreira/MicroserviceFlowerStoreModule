31e94460f0728935fb6ee27717f3a748922f8017
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-06 22:29:38 -0300

    Implementing OpenFeign with FornecedorClient on CompraService

--- src/main/java/br/com/personal/microservice/store/service/CompraService.java
+++ src/main/java/br/com/personal/microservice/store/service/CompraService.java
@@ -1,14 +1,22 @@
 package br.com.personal.microservice.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.personal.microservice.store.client.FornecedorClient;
 import br.com.personal.microservice.store.controller.dto.CompraDTO;
+import br.com.personal.microservice.store.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 
+	@Autowired
+	private FornecedorClient fornecedorClient;
+	
 	public void realizaCompra(CompraDTO compra) {
-		
+	
+		InfoFornecedorDTO fornecedorDTO = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		System.out.println(fornecedorDTO.getEndereco());
 	}
 
 }
