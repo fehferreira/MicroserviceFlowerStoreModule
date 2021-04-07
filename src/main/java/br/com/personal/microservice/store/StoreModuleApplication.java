fc2fc73d15e5571955dcdc8580eb094a72c157af
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-06 22:06:59 -0300

    Create a bean to inject RestTemplate

--- src/main/java/br/com/personal/microservice/store/StoreModuleApplication.java
+++ src/main/java/br/com/personal/microservice/store/StoreModuleApplication.java
@@ -2,10 +2,19 @@ package br.com.personal.microservice.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StoreModuleApplication {
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StoreModuleApplication.class, args);
 	}
--- src/main/java/br/com/personal/microservice/store/service/CompraService.java
+++ src/main/java/br/com/personal/microservice/store/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.personal.microservice.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,11 @@ import br.com.personal.microservice.store.controller.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
-		RestTemplate client = new RestTemplate();
+		
 		ResponseEntity<InfoFornecedorDTO> infoFornecedor = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(),HttpMethod.GET,
 				null, InfoFornecedorDTO.class);
 		
